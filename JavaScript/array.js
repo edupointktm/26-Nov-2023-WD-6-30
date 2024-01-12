23b135a0a42baafa1a6eeed078e431ca4ccd315d
edupointktm <edupointktm@gmail.com>
2024-01-12 20:34:19 +0545

    array_object

---
+++ JavaScript/array.js
@@ -0,0 +1,80 @@
+
+// var n0=5
+// var n1=10
+// var n2 =3
+// var n3=6
+// var n4=7
+
+// s=n0+n1+n2+n3+n4
+
+// console.log("Sum = "+n4)
+
+var n = [9, 6, 9, 8, 7, 2]
+
+//access data from array
+var l = n.length
+var sum=0
+for (i = 0; i < l; i++) {
+    sum =sum + n[i]
+}
+document.write("Sum = "+sum)
+
+var large=0
+var smallest=n[0]
+for(){
+
+
+}
+
+
+var evenCount=0
+var oddCount=0
+for()
+
+
+// var student = ['ram', 'gopal', 'hari', 'prabhat', 'ganesh','rupesh', 'sunil']
+// var l = student.length
+
+// for(i=0;i<l;i++){
+//     document.write(`${student[i]} <br>`)
+// }
+
+// Array=> [index and start from 0 index]
+// Object (list)=> {key:value}
+
+// var student = {names:'Prabhat Kumar', address:'Kathmandu', mobile_no:'9844027213'}
+
+// //access data from object
+
+// document.write(`${student.names}  ${student.address}  ${student.mobile_no}`)
+
+//Multiple data store with key and value
+//Arra of object
+// var student = [
+//     {
+//         names: 'Prabhat Kumar',
+//         address: 'Kathmandu',
+//         mobile_no: '9844027213'
+//     },
+//     {
+//         names: 'Gopal Yadav',
+//         address: 'Janakpur',
+//         mobile_no: '548421545'
+//     },
+//     {
+//         names: 'Janak Dhakal',
+//         address: 'Pokhara',
+//         mobile_no: '154545'
+//     },
+//     {
+//         names: 'Rupesh Yadav',
+//         address: 'Moran',
+//         mobile_no: '5454545'
+//     }
+// ]
+// var l = student.length
+// for (i = 0; i < l; i++) {
+//     document.write(`${student[i].names}  ${student[i].address}  ${student[i].mobile_no} <br>`)
+// }
+// console.log(student[2].names)
+
---
+++ JavaScript/sixth.js
@@ -0,0 +1,93 @@
+//// Control Flow
+// 1. Sequence 
+// 2. Selection 
+// 3. Repeatation (Loop)
+// 4. Object
+// 5. DOM
+
+// 2 Selection
+// if
+// if else
+// if else if 
+// nested if else
+
+// syntax 
+
+// if(condition){
+
+
+// // }
+
+// // eg
+
+// var n=4
+
+// if(n >=5){
+//     console.log("Welcome")
+// }
+
+// if(cond){
+
+// }
+// else{
+
+// }
+
+
+
+// var n = -5
+// if(n > 0){
+//     console.log("yes")
+// }
+// else{
+//     console.log("No")
+// }
+// var n = "no"
+// if(n == "ok"){
+//     console.log("I love you")
+// }
+// else{
+//     console.log("Sorry DD")
+// }
+
+// var n = parseInt(prompt("Enter week day number = "))
+// var msg
+// if (n == 1) {
+//     msg ="Sunday"
+// }
+// else if (n == 2) {
+//     msg = "Monday"
+// }
+// else if (n == 3) {
+//     msg = "Tuesday"
+// }
+// else if (n == 4) {
+//     msg = "Wednesday"
+// }
+// else if (n == 5) {
+//     msg = "Thursday"
+// }
+// else if (n == 6) {
+//     msg = "Firday"
+// }
+// else if(n==7){
+//     msg = "Saturday"
+// }
+// else{
+//     msg = "Sorry! you entred number is not week day no."
+// }
+
+
+// document.write(msg)
+
+//WAP to ask any one number and check given number is ODD or EVEN
+// var n=10
+// r = n%2
+// if(r==0){
+//     console.log("EVen")
+// }
+// else{
+//     console.log("ODD")
+// }
+
+//WAP to ask any one number and check given number is Positive and negatvie
